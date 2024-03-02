56f71ac81ec1b7e77ce4193c19e3879551a7643e
daria <daaria.suv@yandex.ru>
2024-03-02 12:12:48 +0200

    stackoverflow client changed

--- scrapper/src/main/java/edu/java/scrapper/clients/StackOverflowWebClient.java
+++ scrapper/src/main/java/edu/java/scrapper/clients/StackOverflowWebClient.java
@@ -3,6 +3,8 @@ package edu.java.scrapper.clients;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import edu.java.scrapper.dto.response.StackOverflowResponse;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -10,7 +12,6 @@ public class StackOverflowWebClient {
 
     @Value("${api.stackoverflow.url}")
     String url;
-
     WebClient webClient;
 
     public StackOverflowWebClient() {
@@ -23,25 +24,28 @@ public class StackOverflowWebClient {
     }
 
     StackOverflowResponse getRecentAnswer(Long questionId) {
-        String request = url + "questions/" + questionId.toString() + "/answers";
+        System.out.println(url);
+        String request = url + "questions/" + questionId + "/answers?site=stackoverflow";
 
-        var x = webClient.get()
-            .uri(uriBuilder -> uriBuilder
-                .path(request)
-                .queryParam("pagesize", 1)
-                .queryParam("order", "desc")
-                .queryParam("sort", "activity")
-                .queryParam("site", "stackoverflow")
-                .build()
-            )
-            .retrieve();
+        String req = "https://api.stackexchange.com/2.3/" + "questions/" + questionId + "/answers?site=stackoverflow&pagesize=1&order=desc&sort=activity";
 
-        var json = x
+
+        var json = webClient.get()
+//            .uri(uriBuilder -> uriBuilder
+//                .path(request)
+//                .queryParam("pagesize", 1)
+//                .queryParam("order", "desc")
+//                .queryParam("sort", "activity")
+//                .queryParam("site", "stackoverflow")
+//                .build(questionId)
+//            )
+            .uri(request)
+            .retrieve()
             .bodyToMono(String.class)
-            .mapNotNull(this::parse)
+//            .mapNotNull(this::parse)
             .block();
-
-        return json;
+        System.out.println(json);
+        return null;
     }
 
     StackOverflowResponse parse(String json) {
@@ -56,3 +60,11 @@ public class StackOverflowWebClient {
         }
     }
 }
+//
+//
+//class Main {
+//    public static void main(String[] args) {
+//        var cl = new StackOverflowWebClient();
+//        cl.getRecentAnswer(184618L);
+//    }
+//}
