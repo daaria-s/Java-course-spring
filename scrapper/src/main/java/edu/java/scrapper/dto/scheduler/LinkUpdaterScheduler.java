bc6453a086e99d2ffb46df17c8c459047bacd2f3
daria <daaria.suv@yandex.ru>
2024-03-03 18:50:45 +0200

    scheduler added

---
+++ scrapper/src/main/java/edu/java/scrapper/dto/scheduler/LinkUpdaterScheduler.java
@@ -0,0 +1,15 @@
+package edu.java.scrapper.dto.scheduler;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class LinkUpdaterScheduler {
+
+    @Scheduled(fixedDelayString = "${app.scheduler.interval}")
+    public void update() {
+        log.info("Running update");
+    }
+}
